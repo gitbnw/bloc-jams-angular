db92b5574ab02478b8302691a9b53f0d5b2ff166
gitbnw <byron.weiss@gmail.com>
2016-07-22 09:32:47 -0500

    angular assign 7 done

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -26,6 +26,11 @@
 
              currentSong = song;
          };
+         /**
+          * @function SongPlayer.play
+          * @desc Plays or pauses song depending on current playing state
+          * @param {Object} song
+          */
          SongPlayer.play = function(song) {
 
              if (currentSong !== song) {
@@ -49,12 +54,20 @@
                  }
              }
          };
-
+         /**
+          * @function SongPlayer.pause
+          * @desc Pauses song
+          * @param {Object} song
+          */
          SongPlayer.pause = function(song) {
              currentBuzzObject.pause();
              song.playing = false;
          };
-
+         /**
+          * @function SongPlayer.playSong
+          * @desc Plays song
+          * @param {Object} song
+          */
          SongPlayer.playSong = function(song){
            currentBuzzObject.play();
            song.playing = true;
