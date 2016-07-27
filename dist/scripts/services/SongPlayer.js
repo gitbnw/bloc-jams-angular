1b10485a4a4597c37de3325c64a3c5df9b9786fa
gitbnw <byron.weiss@gmail.com>
2016-07-26 21:06:29 -0500

    ang cp 10 done

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
  (function() {
-     function SongPlayer(Fixtures) {
+     function SongPlayer($rootScope, Fixtures) {
          var SongPlayer = {};
          /**
           * @desc album data
@@ -35,9 +35,15 @@
                  formats: ['mp3'],
                  preload: true
              });
+             currentBuzzObject.bind('timeupdate', function() {
+                 $rootScope.$apply(function() {
+                     SongPlayer.currentTime = currentBuzzObject.getTime();
+                 });
+             });
 
              SongPlayer.currentSong = song;
          };
+
          /**
           * @function playSong
           * @desc Plays song
@@ -66,6 +72,11 @@
              return currentAlbum.songs.indexOf(song);
          };
          SongPlayer.currentSong = null;
+         /**
+          * @desc Current playback time (in seconds) of currently playing song
+          * @type {Number}
+          */
+         SongPlayer.currentTime = null;
          /**
           * @function SongPlayer.play
           * @desc Plays or pauses song depending on current playing state
@@ -135,10 +146,20 @@
                  playSong(song);
              }
          };
+         /**
+          * @function setCurrentTime
+          * @desc Set current time (in seconds) of currently playing song
+          * @param {Number} time
+          */
+         SongPlayer.setCurrentTime = function(time) {
+             if (currentBuzzObject) {
+                 currentBuzzObject.setTime(time);
+             }
+         };
          return SongPlayer;
      }
 
      angular
          .module('blocJams')
-         .factory('SongPlayer', SongPlayer);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
