e13e3124a7957c5b61e25c2f5d37aedfe7b54d66
gitbnw <byron.weiss@gmail.com>
2016-07-31 18:46:12 -0500

    ang assign 11 done

--- dist/scripts/filters/timecode.js
+++ dist/scripts/filters/timecode.js
@@ -4,18 +4,20 @@
             if (Number.isNaN(seconds)) {
                 return '-:--';
             }
-            var seconds = Number.parseFloat(seconds);
-            var wholeSeconds = Math.floor(seconds);
-            var minutes = Math.floor(wholeSeconds / 60);
-            var remainingSeconds = wholeSeconds % 60;
+            // var seconds = Number.parseFloat(seconds);
+            // var wholeSeconds = Math.floor(seconds);
+            // var minutes = Math.floor(wholeSeconds / 60);
+            // var remainingSeconds = wholeSeconds % 60;
+            //
+            // var output = minutes + ':';
+            //
+            // if (remainingSeconds < 10) {
+            //     output += '0';
+            // }
+            //
+            // output += remainingSeconds;
 
-            var output = minutes + ':';
-
-            if (remainingSeconds < 10) {
-                output += '0';
-            }
-
-            output += remainingSeconds;
+            output = buzz.toTimer(seconds);
 
             return output;
         };
